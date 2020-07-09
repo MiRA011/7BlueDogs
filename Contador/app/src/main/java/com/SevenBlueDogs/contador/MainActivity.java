83dc411ba8d215cf97503f207fdd359dd53aa938
MiRA011 <mira@alu.uma.es>
2020-07-09 21:07:03 +0200

    añadido reseteo y checkbox

--- Contador/app/src/main/java/com/SevenBlueDogs/contador/MainActivity.java
+++ Contador/app/src/main/java/com/SevenBlueDogs/contador/MainActivity.java
@@ -3,36 +3,61 @@ package com.SevenBlueDogs.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     public int contador;
+    public TextView cuenta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        cuenta=(TextView) findViewById(R.id.tCuenta);
         contador=0;
     }
 
     public void incrementaContador(View vista){
         contador++;
-        mostrarResultado();
+        cuenta.setText(String.valueOf(contador));
     }
 
     public void restaContador(View vista){
         contador--;
-        mostrarResultado();
+        if(contador<0){
+            CheckBox negativos= (CheckBox) findViewById(R.id.negativo);
+            if(!negativos.isChecked()){
+                contador=0;
+            }
+        }
+        cuenta.setText(String.valueOf(contador));
+
 
     }
 
     public void reseteaContador(View vista){
-        contador=0;
-        mostrarResultado();
-    }
+        EditText n_reset= (EditText) findViewById(R.id.n_reset);
 
-    public void mostrarResultado(){
-        TextView cuenta = (TextView) findViewById(R.id.tCuenta);
+        try {
+            contador = Integer.parseInt(n_reset.getText().toString());
+        }catch (Exception e){
+            contador=0;
+        }
+        n_reset.setText("");
         cuenta.setText(String.valueOf(contador));
     }
+
+    public void checkbox(View vista){
+        if(contador<0){
+            CheckBox negativos= (CheckBox) findViewById(R.id.negativo);
+            if(!negativos.isChecked()){
+                contador=0;
+                cuenta.setText(String.valueOf(contador));
+            }
+        }
+
+
+    }
+
 }
