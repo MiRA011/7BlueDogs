efb39ee209b3615d3ccac660c0a10cc2a8225129
MiRA011 <mira@alu.uma.es>
2020-07-09 23:42:34 +0200

    bug de reset solucionado y optimizacion del codigo

--- Contador/app/src/main/java/com/SevenBlueDogs/contador/MainActivity.java
+++ Contador/app/src/main/java/com/SevenBlueDogs/contador/MainActivity.java
@@ -10,54 +10,51 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     public int contador;
     public TextView cuenta;
+    public CheckBox negativos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cuenta=(TextView) findViewById(R.id.tCuenta);
+        negativos= (CheckBox) findViewById(R.id.negativo);
         contador=0;
     }
 
     public void incrementaContador(View vista){
         contador++;
-        cuenta.setText(String.valueOf(contador));
+        escribir();
     }
 
     public void restaContador(View vista){
         contador--;
-        if(contador<0){
-            CheckBox negativos= (CheckBox) findViewById(R.id.negativo);
-            if(!negativos.isChecked()){
-                contador=0;
-            }
-        }
-        cuenta.setText(String.valueOf(contador));
-
-
+        verificar();
+        escribir();
     }
 
     public void reseteaContador(View vista){
         EditText n_reset= (EditText) findViewById(R.id.n_reset);
-
         try {
             contador = Integer.parseInt(n_reset.getText().toString());
         }catch (Exception e){
             contador=0;
         }
+        verificar();
         n_reset.setText("");
-        cuenta.setText(String.valueOf(contador));
+        escribir();
     }
 
     public void checkbox(View vista){
-        if(contador<0){
-            CheckBox negativos= (CheckBox) findViewById(R.id.negativo);
-            if(!negativos.isChecked()){
+        verificar();
+        escribir();
+    }
+
+    public void verificar(){
+        if(contador<0 && !negativos.isChecked()){
                 contador=0;
-                cuenta.setText(String.valueOf(contador));
-            }
         }
-
-
+    }
+    public void escribir(){
+        cuenta.setText(String.valueOf(contador));
     }
 
 }
