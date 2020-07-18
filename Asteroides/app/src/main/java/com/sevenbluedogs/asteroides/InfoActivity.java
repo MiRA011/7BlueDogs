f35b816f6b8eac3a21476a1a818638915164e4ae
MiRA011 <mira@alu.uma.es>
2020-07-18 19:53:50 +0200

    nueva actividad

---
+++ Asteroides/app/src/main/java/com/sevenbluedogs/asteroides/InfoActivity.java
@@ -0,0 +1,14 @@
+package com.sevenbluedogs.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class InfoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
