0a965ba60aead81273e82b3ec1d905554afc0c1c
MiRA011 <mira@alu.uma.es>
2020-07-19 18:44:30 +0200

    info

--- Asteroides/app/src/main/java/com/sevenbluedogs/asteroides/MainActivity.java
+++ Asteroides/app/src/main/java/com/sevenbluedogs/asteroides/MainActivity.java
@@ -18,4 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, InfoActivity.class);
         startActivity(i);
     }
+
+    public void salirApp(View view){
+        finish();
+    }
 }
