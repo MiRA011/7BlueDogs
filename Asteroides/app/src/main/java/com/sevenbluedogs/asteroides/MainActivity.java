c5c16a4463fb3f9875dcf239e1dd0000eb754042
MiRA011 <mira@alu.uma.es>
2020-07-22 18:58:31 +0200

    toolbar

--- Asteroides/app/src/main/java/com/sevenbluedogs/asteroides/MainActivity.java
+++ Asteroides/app/src/main/java/com/sevenbluedogs/asteroides/MainActivity.java
@@ -1,17 +1,26 @@
 package com.sevenbluedogs.asteroides;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     public void ejecutarInfo(View view){
@@ -22,4 +31,22 @@ public class MainActivity extends AppCompatActivity {
     public void salirApp(View view){
         finish();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.configuracion){
+            return true;
+        }else if(id==R.id.info){
+            ejecutarInfo(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
