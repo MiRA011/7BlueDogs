4082c5d40c74b8ec8d525c8bde1c83a13f51b81d
MiRA011 <mira@alu.uma.es>
2020-07-26 20:12:54 +0200

    fin de semana

--- TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
@@ -4,12 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private int jugadores;
+    private int[] casillas;
+    private Partida partida;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializar el array casillas con los identificadores de cada casilla
+        casillas= new int[9];
+        casillas[0]= R.id.cA1;
+        casillas[0]= R.id.cA2;
+        casillas[0]= R.id.cA3;
+        casillas[0]= R.id.cB1;
+        casillas[0]= R.id.cB2;
+        casillas[0]= R.id.cB3;
+        casillas[0]= R.id.cC1;
+        casillas[0]= R.id.cC2;
+        casillas[0]= R.id.cC3;
+    }
+
+    public void empezarPartida(View view){
+        ImageView imagen;
+
+        for (int i : casillas){
+            imagen = (ImageView)findViewById((i));
+            imagen.setImageResource(R.drawable.casilla);
+        }
+
+        int boton =view.getId();
+        if(boton==R.id.bJugador1){
+
+        }else if(boton==R.id.bJugador2){
+
+        }
     }
 }
---
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/Partida.java
@@ -0,0 +1,4 @@
+package com.sevenbluedogs.tresenraya;
+
+public class Partida {
+}
