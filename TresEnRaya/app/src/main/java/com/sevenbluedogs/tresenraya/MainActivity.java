9bdb7f2fb5b3933a8f7f2a02ba141d56342b31bc
Player44574 <djpedrolo2006@gmail.com>
2020-07-29 20:09:13 +0200

    funcional

--- TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
@@ -4,46 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
-    private int jugadores;
-    private int[] casillas;
-    private Partida partida;
-
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Inicializar el array casillas con los identificadores de cada casilla
         casillas= new int[9];
-        casillas[0]= R.id.cA1;
-        casillas[0]= R.id.cA2;
-        casillas[0]= R.id.cA3;
-        casillas[0]= R.id.cB1;
-        casillas[0]= R.id.cB2;
-        casillas[0]= R.id.cB3;
-        casillas[0]= R.id.cC1;
-        casillas[0]= R.id.cC2;
-        casillas[0]= R.id.cC3;
+
+        casillas[0]=R.id.a1;
+        casillas[1]=R.id.a2;
+        casillas[2]=R.id.a3;
+        casillas[3]=R.id.b1;
+        casillas[4]=R.id.b2;
+        casillas[5]=R.id.b3;
+        casillas[6]=R.id.c1;
+        casillas[7]=R.id.c2;
+        casillas[8]=R.id.c3;
     }
 
-    public void empezarPartida(View view){
+    public void empezarPartida(View vista){
+
         ImageView imagen;
 
-        for (int i : casillas){
-            imagen = (ImageView)findViewById((i));
-            imagen.setImageResource(R.drawable.casilla);
+            for(int cadaCasilla: casillas){
+
+                imagen=(ImageView)findViewById((cadaCasilla));
+
+                imagen.setImageResource(R.drawable.casilla);
+
+            }
+
+            jugadores=1;
+
+            if(vista.getId()==R.id.dosjug) {
+
+                jugadores = 2;
+            }
+
+        RadioGroup configDificultad=(RadioGroup)findViewById(R.id.configd);
+
+        int id=configDificultad.getCheckedRadioButtonId();
+
+        int dificultad=0;
+
+        if (id==R.id.normal){
+
+            dificultad=1;
+        }else if(id==R.id.imposible){
+
+            dificultad=2;
+
         }
 
-        int boton =view.getId();
-        if(boton==R.id.bJugador1){
+        partida= new Partida(dificultad);
 
-        }else if(boton==R.id.bJugador2){
+        ( (Button)findViewById(R.id.bJugador1)).setEnabled(false);
 
-        }
+        ((RadioGroup)findViewById(R.id.configd)).setAlpha(0);
+
+        ( (Button)findViewById(R.id.dosjug)).setEnabled(false);
+
     }
+
+    private int jugadores;
+
+    private int[] casillas;
+
+    private Partida partida;
+
+
 }
--- TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/Partida.java
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/Partida.java
@@ -1,4 +1,11 @@
 package com.sevenbluedogs.tresenraya;
 
 public class Partida {
-}
+
+    public final int dificultad;
+
+    public Partida (int dificultad){
+
+        this.dificultad=dificultad; }
+
+    }
