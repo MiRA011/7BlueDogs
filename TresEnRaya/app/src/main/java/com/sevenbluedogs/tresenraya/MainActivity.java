2b88eed3fa6275bb9e636096ba2d825163cfb662
MiRA011 <mira@alu.uma.es>
2020-08-03 06:32:25 +0200

    aplicacion de Tres en Raya

--- TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/MainActivity.java
@@ -1,16 +1,17 @@
 package com.sevenbluedogs.tresenraya;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private int[] casillas;
+    private Partida partida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,54 +32,115 @@ public class MainActivity extends Activity {
     }
 
     public void empezarPartida(View vista){
-
+        //Borramos las jugadas que haya en el tablero
         ImageView imagen;
-
-            for(int cadaCasilla: casillas){
-
-                imagen=(ImageView)findViewById((cadaCasilla));
-
-                imagen.setImageResource(R.drawable.casilla);
-
-            }
-
-            jugadores=1;
-
-            if(vista.getId()==R.id.dosjug) {
-
-                jugadores = 2;
-            }
-
-        RadioGroup configDificultad=(RadioGroup)findViewById(R.id.configd);
-
-        int id=configDificultad.getCheckedRadioButtonId();
-
-        int dificultad=0;
-
-        if (id==R.id.normal){
-
-            dificultad=1;
-        }else if(id==R.id.imposible){
-
-            dificultad=2;
-
+        for(int cadaCasilla: casillas){
+            imagen=(ImageView)findViewById((cadaCasilla));
+            imagen.setImageResource(R.drawable.casilla);
         }
 
-        partida= new Partida(dificultad);
+        int jugador=1;
+        if(vista.getId()==R.id.bJugador2) {
+            jugador = 2;
+        }
 
-        ( (Button)findViewById(R.id.bJugador1)).setEnabled(false);
 
-        ((RadioGroup)findViewById(R.id.configd)).setAlpha(0);
+        RadioGroup configDificultad=(RadioGroup)findViewById(R.id.RadioButton);
+        int id=configDificultad.getCheckedRadioButtonId();
+        int dificultad=0;
+        if (id==R.id.normal){
+            dificultad=1;
+        }else if(id==R.id.imposible){
+            dificultad=2;
+        }
 
-        ( (Button)findViewById(R.id.dosjug)).setEnabled(false);
+        partida= new Partida(dificultad,jugador,casillas);
+        ((Button)findViewById(R.id.bJugador1)).setEnabled(false);
+        ((RadioGroup)findViewById(R.id.RadioButton)).setAlpha(0);
+        ((Button)findViewById(R.id.bJugador2)).setEnabled(false);
 
     }
 
-    private int jugadores;
+   public void nuevaJugada(View view){
 
-    private int[] casillas;
+       if(!(partida==null)) {
+           int casilla = view.getId();
+           boolean encontrado=false;
+           int i=0;
+           while(!encontrado & i<casillas.length){
+               if(casilla==casillas[i]){
+                   encontrado=true;
+                   casilla=i;
+               }
+               i++;
+           }
+           int jugador= partida.jugador;
+           marcar(casilla);
+           int estado=partida.estadoPartida(jugador);
+           if(estado>0){
+               terminarJuego(estado);
+               return;
+           }
 
-    private Partida partida;
+           if(partida.nJugadores==1){
+               marcar(0);
+               estado=partida.estadoPartida(2);
+               if(estado>0){
+                   terminarJuego(estado);
+                   return;
+               }
+           }
+       }
+
+   }
+
+
+    //Marca en el tablero la casilla correspondiente,
+    //si es el turno del J1 marca la casilla que recibe por parametros
+    public void marcar(int casillaj1){
+        ImageView imagen;
+
+        if(partida.jugador==1){
+            if(partida.casillaLibre(casillaj1)){
+                partida.realizarJugada(casillaj1);
+                imagen= (ImageView)findViewById(casillas[casillaj1]);
+                imagen.setImageResource(R.drawable.circulo);
+            }
+        }else if(partida.jugador==2){
+            int casilla=casillaj1;
+            if(partida.nJugadores==1){
+                casilla =partida.ia();
+                while(!partida.casillaLibre(casilla)){
+                    casilla=partida.ia();
+                }
+            }
+            partida.realizarJugada(casilla);
+            imagen= (ImageView)findViewById(casillas[casilla]);
+            imagen.setImageResource(R.drawable.aspa);
+        }
+    }
+
+    public void terminarJuego(int resultado){
+        Toast toast ;
+
+        if(resultado==3){
+            toast =Toast.makeText(this, R.string.empate, Toast.LENGTH_LONG);
+        }
+        else if(resultado==1){
+            toast = Toast.makeText(this, R.string.circulos_ganan, Toast.LENGTH_LONG);
+        }
+        else{
+            toast = Toast.makeText(this, R.string.cruces_ganan, Toast.LENGTH_LONG);
+        }
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        partida=null;
+        ( (Button)findViewById(R.id.bJugador1)).setEnabled(true);
+        ((RadioGroup)findViewById(R.id.RadioButton)).setAlpha(1);
+        ( (Button)findViewById(R.id.bJugador2)).setEnabled(true);
+    }
+
 
 
 }
--- TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/Partida.java
+++ TresEnRaya/app/src/main/java/com/sevenbluedogs/tresenraya/Partida.java
@@ -1,11 +1,127 @@
 package com.sevenbluedogs.tresenraya;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ImageView;
+import java.util.Random;
+
 public class Partida {
 
     public final int dificultad;
+    public int nJugadores;
+    public int jugador; //Jugador al que le pertenece el turno
+    private final int [] TABLERO;  //Almacena los id de las casillas
+    private int [] casillas; //Almacena los resultados en el tablero
+    private final int [][] COMBINACIONES ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
-    public Partida (int dificultad){
+    public Partida (int dificultad,int nJugadores, int [] TABLERO){
+        this.dificultad=dificultad;
+        this.nJugadores=nJugadores;
+        this.TABLERO=TABLERO;
+        jugador=1;
 
-        this.dificultad=dificultad; }
+        //Inicializamos las casillas vacias
+        casillas = new int[9];
+        for (int i =0; i<9; i++ ) {
+            casillas[i]=0;
+        }
+    }
+
+    //Devuelve si existe una casilla vacia con posibilidad de tres en raya para el jugador seleccionado
+    //En caso de no existir devuelve -1;
+    private int dosEnRaya(int jugador){
+        int casilla = -1;
+        int contador = 0;
+
+        for(int i=0; i<COMBINACIONES.length;i++){
+            for(int pos:COMBINACIONES[i]){
+                if(casillas[pos]==jugador){
+                    contador++;
+                }
+
+                if(casillas[pos]==0) casilla=pos;
+            }
+            if(contador==2 && casilla!=-1) return casilla;
+            casilla=-1;
+            contador=0;
+        }
+        return -1;
+    }
+
+    //Simula al J2 con varios niveles de dificultad
+    public int ia(){
+        int casilla;
+        casilla=dosEnRaya(2);
+        if(casilla!=-1)return casilla;
+
+        if(dificultad>0) {
+            casilla = dosEnRaya(1);
+            if (casilla != -1) return casilla;
+        }
+
+        if(dificultad==2){
+            if(casillas[0]==0){return 0;}
+            if(casillas[2]==0){return 2;}
+            if(casillas[6]==0){return 6;}
+            if(casillas[8]==0){return 8;}
+        }
+
+        Random rnd =new Random();
+        casilla = rnd.nextInt(9);
+        return casilla;
+    }
+
+    //Cambia el turno de un jugador a otro
+    private void turno(){
+        jugador++;
+        if(jugador>2){
+            jugador=1;
+        }
+    }
+
+    //Esta funcion devuelve el estado actual de la partida (Debe invocarse antes de cambiar el turno)
+    //Si devuelve 0 la partida no ha terminado
+    //Si devuelve 1 o 2 ha ganado uno de los dos jugadores
+    //Si devuelve 3 la partida ha terminado en empate
+    public int estadoPartida(int jug){
+        boolean empate = true;
+        boolean ult_movimiento=true;
+
+        for(int i=0; i<COMBINACIONES.length;i++){
+            for(int pos:COMBINACIONES[i]){
+
+                if(casillas[pos]!=jug)ult_movimiento=false;
+
+                if(casillas[pos]==0)empate=false;
+            }
+            if(ult_movimiento) return jug;
+            ult_movimiento=true;
+        }
+
+        if(empate)return 3;
+
+        return 0;
+    }
+
+    //Comprueba si la casilla esta libre
+    public boolean casillaLibre(int casilla){
+        if(casillas[casilla]!=0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //Marca en el tablero la casilla correspondiente, y avanza el turno
+    public void realizarJugada(int casilla){
+        casillas[casilla]=this.jugador;
+        turno();
+    }
 
     }
