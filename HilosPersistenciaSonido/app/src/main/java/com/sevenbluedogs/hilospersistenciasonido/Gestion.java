a467a8a8a753d5f870f5ff2f44f39ff1175d42e8
MiRA011 <mira@alu.uma.es>
2020-08-06 13:45:53 +0200

    actualizacion de contenidos

--- HilosPersistenciaSonido/app/src/main/java/com/sevenbluedogs/hilospersistenciasonido/Gestion.java
+++ HilosPersistenciaSonido/app/src/main/java/com/sevenbluedogs/hilospersistenciasonido/Gestion.java
@@ -3,6 +3,7 @@ package com.sevenbluedogs.hilospersistenciasonido;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
@@ -14,11 +15,15 @@ public class Gestion extends AppCompatActivity {
     private int dificultad;
     private final int FPS=30;
     private Handler temporizador;
+    private MediaPlayer golpeo;
+    private MediaPlayer fin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         botes=0;
+        golpeo = MediaPlayer.create(this, R.raw.toque);
+        fin = MediaPlayer.create(this,R.raw.finalpartida);
         setContentView(R.layout.activity_gestion);
         Bundle extras = getIntent().getExtras();
         dificultad = extras.getInt("dificultad");
@@ -44,11 +49,12 @@ public class Gestion extends AppCompatActivity {
         int x = (int) evento.getX();
         int y = (int) evento.getY();
         if (partida.toque(x,y)) botes++;
+        golpeo.start();
         return false;
     }
 
     public void fin(){
-
+        fin.start();
         temporizador.removeCallbacks(hilo);
         Intent in=new Intent();
         in.putExtra("puntuacion", botes);
