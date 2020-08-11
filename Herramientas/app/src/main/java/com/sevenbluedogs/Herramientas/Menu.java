b36fffea148623bb25b0a4fce961e97631890d84
Player44574 <djpedrolo2006@gmail.com>
2020-08-11 18:48:39 +0200

    otra update herramientas

--- Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/Menu.java
+++ Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/Menu.java
@@ -1,5 +1,6 @@
 package com.sevenbluedogs.Herramientas;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,9 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class Menu extends Fragment {
 
+    public final int[] BOTONESMENU={R.id.linterna, R.id.musica,R.id.nivel};
 
     public Menu() {
 
@@ -19,6 +22,29 @@ public class Menu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View mimenu= inflater.inflate(R.layout.fragment_menu, container, false);
+
+        ImageButton botonmenu;
+
+        for(int i=0;i<BOTONESMENU.length;i++){
+
+            botonmenu= (ImageButton) mimenu.findViewById(BOTONESMENU[i]);
+
+            final int queBoton=i;
+
+            botonmenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Activity estaActividad=getActivity();
+
+                    ((ComunicaMenu)estaActividad).menu(queBoton);
+
+                }
+            });
+
+        }
+
+        return mimenu;
     }
 }
