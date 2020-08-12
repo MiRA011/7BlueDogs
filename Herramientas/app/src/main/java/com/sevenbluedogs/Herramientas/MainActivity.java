3c7fa5731eded799aa68ee97a31bffca0c21c9cf
MiRA011 <mira@alu.uma.es>
2020-08-12 12:41:47 +0200

    fragmentos

--- Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/MainActivity.java
+++ Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/MainActivity.java
@@ -2,13 +2,22 @@ package com.sevenbluedogs.Herramientas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ComunicaMenu {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void menu(int boton) {
+        Intent in = new Intent(this, ActividadHerramientas.class);
+        in.putExtra("idBoton",boton);
+        startActivity(in);
+
     }
 }
