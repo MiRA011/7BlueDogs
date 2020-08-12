83191559c4f4e83b5b273b651ae6c001f22bebf8
Player44574 <djpedrolo2006@gmail.com>
2020-08-12 13:11:53 +0200

    AAA

--- Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/ActividadHerramientas.java
+++ Herramientas/app/src/main/java/com/sevenbluedogs/Herramientas/ActividadHerramientas.java
@@ -1,14 +1,42 @@
 package com.sevenbluedogs.Herramientas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class ActividadHerramientas extends AppCompatActivity {
+public class ActividadHerramientas extends AppCompatActivity implements ComunicaMenu {
+
+    Fragment [] misFragmentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_herramientas);
+
+        misFragmentos=new Fragment[3];
+
+        misFragmentos[0]=new Linterna();
+        misFragmentos[1]=new Musica();
+        misFragmentos[2]=new Nivel();
+
+        Bundle extras=getIntent().getExtras();
+
+        menu(extras.getInt("idBoton"));
+    }
+
+    @Override
+    public void menu(int queboton){
+
+        FragmentManager miManejador=getSupportFragmentManager();
+
+        FragmentTransaction miTransaccion=miManejador.beginTransaction();
+
+        miTransaccion.replace(R.id.herramientas, misFragmentos[queboton] );
+
+        miTransaccion.commit();
+
     }
 }
