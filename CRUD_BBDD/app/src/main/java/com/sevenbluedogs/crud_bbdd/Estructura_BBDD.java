3ff7dbe13c0e61a25645c64cd78a228dc5cb81b5
Player44574 <djpedrolo2006@gmail.com>
2020-08-18 14:05:45 +0200

    Base de datos

---
+++ CRUD_BBDD/app/src/main/java/com/sevenbluedogs/crud_bbdd/Estructura_BBDD.java
@@ -0,0 +1,17 @@
+package com.sevenbluedogs.crud_bbdd;
+
+import android.provider.BaseColumns;
+
+public class Estructura_BBDD {
+
+    private Estructura_BBDD(){}
+    //public static class FeedEntry implements BaseColumns{
+
+        public static final String TABLE_NAME = "datosPersonales";
+        public static final String NOMBRE_COLUMNA1 = "Id";
+        public static final String NOMBRE_COLUMNA2 = "Nombre";
+        public static final String NOMBRE_COLUMNA3 = "Apellido";
+
+    //}
+
+}
