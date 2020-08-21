fd3cf09677536e23156e3e967753762133c1a062
Player44574 <djpedrolo2006@gmail.com>
2020-08-21 19:33:49 +0200

    update BBDD

--- CRUD_BBDD/app/src/main/java/com/sevenbluedogs/crud_bbdd/MainActivity.java
+++ CRUD_BBDD/app/src/main/java/com/sevenbluedogs/crud_bbdd/MainActivity.java
@@ -2,10 +2,15 @@ package com.sevenbluedogs.crud_bbdd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +32,21 @@ public class MainActivity extends AppCompatActivity {
         textoNombre = (EditText)findViewById(R.id.nombre);
         textoApellido = (EditText)findViewById(R.id.apellido);
 
+        final BBDD_Helper helper = new BBDD_Helper(this);
+
         botonInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SQLiteDatabase db = helper.getWritableDatabase();
 
+                ContentValues values = new ContentValues();
+                values.put(Estructura_BBDD.NOMBRE_COLUMNA1, textoId.getText().toString());
+                values.put(Estructura_BBDD.NOMBRE_COLUMNA2, textoNombre.getText().toString());
+                values.put(Estructura_BBDD.NOMBRE_COLUMNA3, textoApellido.getText().toString());
+
+                long newRowId = db.insert(Estructura_BBDD.TABLE_NAME,  null, values);
+
+                Toast.makeText(getApplicationContext(), "Se guardó el registro con clave:" + newRowId, Toast.LENGTH_LONG).show();
             }
         });
 
