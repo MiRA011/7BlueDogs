ea1c5a5d51a203ff93ba348ce0d2b8526467343e
Player44574 <djpedrolo2006@gmail.com>
2020-08-29 12:53:06 +0200

    Buscador java

--- RutasMetroMadrid/app/src/main/java/com/sevenbluedogs/rutasmetromadrid/Buscador.java
+++ RutasMetroMadrid/app/src/main/java/com/sevenbluedogs/rutasmetromadrid/Buscador.java
@@ -2,6 +2,12 @@ package com.sevenbluedogs.rutasmetromadrid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
@@ -10,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import java.net.NetworkInterface;
 import java.sql.Array;
 import java.util.Arrays;
 
@@ -36,28 +43,71 @@ public class Buscador extends AppCompatActivity {
         origen.setText("");
         destino = (EditText) findViewById(R.id.destino);
         destino.setText("");
-        enviar =  (Button) findViewById(R.id.enviar);
+        enviar = (Button) findViewById(R.id.enviar);
         enviar.setAlpha(1);
         enviar.setEnabled(true);
-        if(lineas==null){
+        if (lineas == null) {
             Bundle miBundle = getIntent().getExtras();
             Parcelable[] datos = miBundle.getParcelableArray("LINEAS");
-            lineas= Arrays.copyOf(datos, datos.length,Lineas[].class);
+            lineas = Arrays.copyOf(datos, datos.length, Lineas[].class);
         }
+
+        rutas = new Rutas();
     }
 
-    public void leeDirecciones(View view){
+    public void leeDirecciones(View view) {
         enviar.setAlpha(0);
         enviar.setEnabled(false);
 
-        direccionOrigen=origen.getText().toString();
-        direccionDestino=destino.getText().toString();
+        direccionOrigen = origen.getText().toString();
+        direccionDestino = destino.getText().toString();
 
         InputMethodManager introduce = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        introduce.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+        introduce.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
-        barraProgreso= new ProgressBar(this);
+        barraProgreso = new ProgressBar(this);
+        ejecutaSegundoPlano tarea= new ejecutaSegundoPlano();
+        tarea.execute();
 
     }
 
+    private class ejecutaSegundoPlano extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Location puntoOringen;
+            Location puntoDestino;
+            Context context = getApplicationContext();
+            ConnectivityManager miManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo estadoRed = miManager.getActiveNetworkInfo();
+            if (estadoRed == null || !estadoRed.isConnected() || !estadoRed.isAvailable()) {
+                return getString(R.string.error_conexion);
+            }
+            try {
+                puntoOringen = OptimizacionBusqueda.busca(direccionOrigen);
+                if (puntoOringen == null) return getString(R.string.error_origen);
+                puntoDestino = OptimizacionBusqueda.busca(direccionDestino);
+                if (puntoDestino == null) return getString(R.string.error_destino);
+            } catch (Exception Manolo) {
+                return getString(R.string.error_red);
+            }
+            rutas.mejorRuta(puntoOringen, puntoDestino, lineas);
+            return null;
+        }
+
+        protected void onPostExecute(String resultado) {
+            barraProgreso = null;
+            enviar.setAlpha(1);
+            enviar.setEnabled(true);
+            muestraRuta();
+        }
+
+        public void muestraRuta() {
+
+        }
+    }
+    public void onBackPressed(){
+        if(barraProgreso!=null) barraProgreso=null;
+        moveTaskToBack(true);
+    }
 }
